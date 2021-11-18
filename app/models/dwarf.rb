6183c9b89fd6136266cc9391b7a52183c1527c51
Nicolas Dubois <duboisnicolas98@gmail.com>
2021-11-18 17:52:27 +0100

    Cards

--- app/models/dwarf.rb
+++ app/models/dwarf.rb
@@ -1,6 +1,6 @@
 class Dwarf < ApplicationRecord
   belongs_to :user
-  has_many :offers
+  has_many :offers, dependent: :destroy
   has_one_attached :picture
   geocoded_by :localisation
   after_validation :geocode, if: :will_save_change_to_localisation?
